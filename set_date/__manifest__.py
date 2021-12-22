47373da1d9052056485199f589c1304612226ab5
kitoo08 <myinthtun08@gmail.com>
2021-12-22 17:23:50 +0630

    add price

--- set_date/__manifest__.py
+++ set_date/__manifest__.py
@@ -13,6 +13,8 @@
     'author': "MarkERP",
     'website': "https://markinnovative.com",
     'license': 'LGPL-3',
+    'price': 10.0,
+    'currency': 'USD',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
